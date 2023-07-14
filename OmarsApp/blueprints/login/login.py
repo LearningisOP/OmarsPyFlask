cb94864e88f5bf7a35e5db7892dff9bd43970ed0
Omar Codes <omariscoding00@gmail.com>
2023-07-13 20:36:28 -0700

    created login folder

---
+++ OmarsApp/blueprints/login/login.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, Flask, render_template, redirect, url_for, request
+
+
+login_bp = Blueprint('login', __name__, template_folder="templates")
+
+@login_bp('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)

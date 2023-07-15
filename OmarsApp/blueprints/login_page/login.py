7a11e6c31c932849b108862ff771ba03b3ba386f
Omar Codes <omariscoding00@gmail.com>
2023-07-15 16:44:23 -0700

    created sign up login and views html

--- OmarsApp/blueprints/login_page/login.py
+++ OmarsApp/blueprints/login_page/login.py
@@ -4,8 +4,17 @@ from flask import Blueprint, render_template,request, redirect, url_for
 login_bp = Blueprint('login', __name__, template_folder="templates")
 
 
-@login_bp.route("/sign-up")
+@login_bp.route("/sign-up", methods=['GET', 'POST'])
+def loginindex():
+    return render_template("signup.html")
+
+@login_bp.route("/login", methods=['GET', 'POST'])
 def loginindex():
     return render_template("login.html")
+
+
+@login_bp.route("/logout", methods=['GET', 'POST'])
+def loginindex():
+    return redirect(url_for("views.html"))
 
 

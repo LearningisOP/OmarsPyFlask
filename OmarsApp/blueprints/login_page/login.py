7f25e877650ea5d3b61a950634a951c066d37376
Omar Codes <omariscoding00@gmail.com>
2023-07-15 16:56:27 -0700

    added html layout code

--- OmarsApp/blueprints/login_page/login.py
+++ OmarsApp/blueprints/login_page/login.py
@@ -1,20 +1,22 @@
-from flask import Blueprint, render_template,request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 
 
 login_bp = Blueprint('login', __name__, template_folder="templates")
 
 
 @login_bp.route("/sign-up", methods=['GET', 'POST'])
-def loginindex():
+def signup():
+    username = request.form.get("username")
+    print(username)
     return render_template("signup.html")
 
 @login_bp.route("/login", methods=['GET', 'POST'])
-def loginindex():
+def login():
     return render_template("login.html")
 
 
 @login_bp.route("/logout", methods=['GET', 'POST'])
-def loginindex():
+def logout():
     return redirect(url_for("views.html"))
 
 

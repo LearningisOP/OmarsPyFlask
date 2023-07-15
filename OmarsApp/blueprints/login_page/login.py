cd0117c840a8ae9ee49752426c858df2a8dd324e
Omar Codes <omariscoding00@gmail.com>
2023-07-15 16:40:49 -0700

    started /sign up route

--- OmarsApp/blueprints/login_page/login.py
+++ OmarsApp/blueprints/login_page/login.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template,request, redirect, url_for
 login_bp = Blueprint('login', __name__, template_folder="templates")
 
 
-@login_bp.route("/")
+@login_bp.route("/sign-up")
 def loginindex():
     return render_template("login.html")
 

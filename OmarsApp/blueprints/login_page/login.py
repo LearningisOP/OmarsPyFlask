352d7980f7faaff60e9ec69312802046ac6cd82c
Omar Codes <omariscoding00@gmail.com>
2023-08-07 01:05:01 -0700

    background to pwgen, aligned text, and copy text

--- OmarsApp/blueprints/login_page/login.py
+++ OmarsApp/blueprints/login_page/login.py
@@ -1,22 +1,37 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 
 login_bp = Blueprint('login', __name__, template_folder="templates")
 
+REGISTRANTS = {}
+
+REASONS = [
+    "Curiosity"
+    "personalReason"
+    'forfun'
+]
+
 
 @login_bp.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", reason=REASONS)
 
 
 @login_bp.route("/register", methods=['POST'])
 def register():
     name = request.form.get("name")
-    forfun = request.form.get("forfun")
-    personalReason = request.form.get("personalReason")
-    Curiosity = request.form.get("Curiosity")
-    Other = request.form.get("Other")
+    if not name:
+        return render_template("login.html")
+    reason = request.form.get("reason")
+    Other = request.form.get("otherRad")
+    if reason not in REASONS:
+        return render_template("login.html")
     return render_template("about.html")
+
+
+@login_bp.route("/registered")
+def registered():
+    return render_template("registrants.html")
 
 
 

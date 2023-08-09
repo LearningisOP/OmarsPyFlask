ebd8cd95da6842e3c38f211967580ec0146cba2a
Omar Codes <omariscoding00@gmail.com>
2023-08-09 01:58:02 -0700

    added login page and background

--- OmarsApp/blueprints/login_page/login.py
+++ OmarsApp/blueprints/login_page/login.py
@@ -1,20 +1,20 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 
-login_bp = Blueprint('login', __name__, template_folder="templates")
+login_bp = Blueprint('login', __name__, template_folder="templates", static_url_path='/login_page/static', static_folder='static')
 
 REGISTRANTS = {}
 
 REASONS = [
-    "Curiosity"
-    "personalReason"
-    'forfun'
+    "Business",
+    "Curiosity",
+    "Family/Friend",
+    'for fun'
 ]
-
 
 @login_bp.route("/")
 def login():
-    return render_template("login.html", reason=REASONS)
+    return render_template("login.html", reasons=REASONS)
 
 
 @login_bp.route("/register", methods=['POST'])
@@ -26,12 +26,17 @@ def register():
     Other = request.form.get("otherRad")
     if reason not in REASONS:
         return render_template("login.html")
-    return render_template("about.html")
+    REGISTRANTS[reason] = reason
+    return render_template("home.html")
 
 
 @login_bp.route("/registered")
 def registered():
-    return render_template("registrants.html")
+    return render_template("registrants.html", registrants=REGISTRANTS)
+
+@login_bp.route("/about")
+def skip():
+    return render_template("home.html")
 
 
 
